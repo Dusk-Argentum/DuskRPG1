34e301cfdf99bce995cdda15a9037e566a06dada
Brandon <dusk.argentum@gmail.com>
2015-10-09 16:33:59 -0400

    CommitThree

--- Program.cs
+++ Program.cs
@@ -43,8 +43,7 @@ namespace DuskRPG1
         {
             ///Introduce the player to the game and ask their name.
             /// 
-            PrintTextLine("|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|07\n");
-
+            PrintTextLine("|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08+|09*|08\n");
             PrintTextLine("Hello! Welcome to DuskRPG, a text-based RPG in its very early stages!");
             PrintText("Welcome, new player! Please enter your name: ");
             string name = Console.ReadLine();
@@ -56,7 +55,7 @@ namespace DuskRPG1
             //Console.ReadKey("n", true);
             PrintTextLine("Normally, you'd be able to type 'yes' or 'no' here, but unfortunately, I am not skilled enough to do this. Sorry.");
 
-            PrintTextLine("In any event, welcome, {0}! There isn't much to do yet.", name);
+            PrintTextLine("In any event, welcome, |09{0}|07! There isn't much to do yet.", name);
             ///Due to me not knowing how to allow users to input and read different options, the remaining bits will be added at a future date.
         }
     }
