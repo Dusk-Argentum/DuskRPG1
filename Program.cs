d4a1d5dc54c5ab949b7e836f493a316bd406f610
Brandon <dusk.argentum@gmail.com>
2015-10-09 12:54:10 -0400

    Commit2

--- Program.cs
+++ Program.cs
@@ -8,23 +8,55 @@ namespace DuskRPG1
 {
     class Program
     {
+        public static void PrintTextLine(String text, params Object[] param)
+        {
+            PrintText(text + "\n", param);
+        }
+
+        public static void PrintText (String text, params Object[] param)
+        {
+            for (int i = 0; i < param.Length; i++)
+            {
+                text = text.Replace("{" + i + "}", param[i].ToString());
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '|')
+                {
+                    int bg = Convert.ToInt32(text[i+1].ToString());
+                    int fg = Convert.ToInt32(text[i+2].ToString(), 16);
+
+                    Console.ForegroundColor = (ConsoleColor)fg;
+                    Console.BackgroundColor = (ConsoleColor)bg;
+
+                    i += 2;
+                }
+                else
+                {
+                    Console.Write(text[i]);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             ///Introduce the player to the game and ask their name.
-            Console.WriteLine("Hello! Welcome to DuskRPG, a text-based RPG in its very early stages!");
-            Console.Write("Welcome, new player! Please enter your name. ");
-            string nameAsString = Console.ReadLine();
+            /// 
+            PrintTextLine("|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|02-|0a=|07\n");
 
-            Console.Write("Ah, so your name is {1}?", name);
+            PrintTextLine("Hello! Welcome to DuskRPG, a text-based RPG in its very early stages!");
+            PrintText("Welcome, new player! Please enter your name: ");
+            string name = Console.ReadLine();
 
+            PrintTextLine("Ah, so your name is |09{0}|07?", name);
             //Console.ReadKey("y", true);
             //bool y = false;
             //string y1String = Console.ReadKey(y);
             //Console.ReadKey("n", true);
+            PrintTextLine("Normally, you'd be able to type 'yes' or 'no' here, but unfortunately, I am not skilled enough to do this. Sorry.");
 
-            Console.WriteLine("Normally, you'd be able to type 'yes' or 'no' here, but unfortunately, I am not skilled enough to do this. Sorry.");
-
-            Console.WriteLine("In any event, welcome, {1}! There isn't much to do yet.");
+            PrintTextLine("In any event, welcome, {0}! There isn't much to do yet.", name);
             ///Due to me not knowing how to allow users to input and read different options, the remaining bits will be added at a future date.
         }
     }
